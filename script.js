86cf12a6407e104973716be99708c670b6d7d7c7
lhoffmeyer89 <lhoffmeyer89@gmail.com>
2020-12-09 21:41:39 -0500

    pushing up products

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+class product {
+    constructor(name, category, description, price) {
+      this.name = name;
+      this.category = category;
+      this.description = description;
+    //   this.image = image;
+      this.price = price;
+    }
+  }
+
+  const cristmasTree = new product("Christmas Tree", "Traditional", "Decorate your house with this beautify traditional style Christmas Tree!", 100);
+  const mask = new product("Christmas Mask", "Traditional", "Protect yourself in style with this Christmas mask!", 20);
 
 
+console.log(cristmasTree);
 
+document.getElementById("products").getElementsByClassName("name")[0].innerHTML = cristmasTree.name;
+document.getElementById("products").getElementsByClassName("category")[0].innerHTML = cristmasTree.category;
+document.getElementById("products").getElementsByClassName("description")[0].innerHTML = cristmasTree.description;
+document.getElementById("products").getElementsByClassName("price")[0].innerHTML = cristmasTree.price;
+
+document.getElementById("mask").getElementsByClassName("name")[0].innerHTML = mask.name;
 
