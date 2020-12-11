0339b9b74474e6d57d16f720ee762c8e793254dc
Brad Story <brad.story@smarterservices.com>
2020-12-10 21:38:17 -0500

    fix merge conflict on main

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 class product {
-    constructor(name, category, description, price) {
-      this.name = name;
-      this.category = category;
-      this.description = description;
-    //   this.image = image;
-      this.price = price;
-    }
+  constructor(name, category, description, price) {
+    this.name = name;
+    this.category = category;
+    this.description = description;
+  //   this.image = image;
+    this.price = price;
   }
+}
 
 const cristmasTree = new product("Christmas Tree", "Traditional", "Decorate your house with this beautify traditional style Christmas Tree!", 100);
 const mask = new product("Christmas Mask", "Traditional", "Protect yourself in style with this Christmas mask!", 20);
