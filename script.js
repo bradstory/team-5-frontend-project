6bb5319a192658dff236fa2ffaf80639508c5812
lhoffmeyer89 <lhoffmeyer89@gmail.com>
2020-12-13 16:20:28 -0500

    pushing again

--- script.js
+++ script.js
@@ -69,118 +69,52 @@ const whiteElephant = new product(
   1000
 );
 
-document
-  .getElementById("christmasTree")
-  .getElementsByClassName("name")[0].innerHTML = cristmasTree.name;
-document
-  .getElementById("christmasTree")
-  .getElementsByClassName("category")[0].innerHTML = cristmasTree.category;
-document
-  .getElementById("christmasTree")
-  .getElementsByClassName("description")[0].innerHTML = cristmasTree.description;
-document
-  .getElementById("christmasTree")
-  .getElementsByClassName("price")[0].innerHTML = cristmasTree.price;
+document.getElementById("christmasTree").getElementsByClassName("name")[0].innerHTML = cristmasTree.name;
+document.getElementById("christmasTree").getElementsByClassName("category")[0].innerHTML = cristmasTree.category;
+document.getElementById("christmasTree").getElementsByClassName("description")[0].innerHTML = cristmasTree.description;
+document.getElementById("christmasTree").getElementsByClassName("price")[0].innerHTML = cristmasTree.price;
 
 document.getElementById("mask").getElementsByClassName("name")[0].innerHTML = mask.name;
-document
-  .getElementById("mask")
-  .getElementsByClassName("category")[0].innerHTML = mask.category;
-document
-  .getElementById("mask")
-  .getElementsByClassName("description")[0].innerHTML = mask.description;
+document.getElementById("mask").getElementsByClassName("category")[0].innerHTML = mask.category;
+document.getElementById("mask").getElementsByClassName("description")[0].innerHTML = mask.description;
 document.getElementById("mask").getElementsByClassName("price")[0].innerHTML = mask.price;
 
-document
-  .getElementById("stocking")
-  .getElementsByClassName("name")[0].innerHTML = stocking.name;
-document
-  .getElementById("stocking")
-  .getElementsByClassName("category")[0].innerHTML = stocking.category;
-document
-  .getElementById("stocking")
-  .getElementsByClassName("description")[0].innerHTML = stocking.description;
-document
-  .getElementById("stocking")
-  .getElementsByClassName("price")[0].innerHTML = stocking.price;
+document.getElementById("stocking").getElementsByClassName("name")[0].innerHTML = stocking.name;
+document.getElementById("stocking").getElementsByClassName("category")[0].innerHTML = stocking.category;
+document.getElementById("stocking").getElementsByClassName("description")[0].innerHTML = stocking.description;
+document.getElementById("stocking").getElementsByClassName("price")[0].innerHTML = stocking.price;
 
-document
-  .getElementById("uglySweater")
-  .getElementsByClassName("name")[0].innerHTML = uglySweater.name;
-document
-  .getElementById("uglySweater")
-  .getElementsByClassName("category")[0].innerHTML = uglySweater.category;
-document
-  .getElementById("uglySweater")
-  .getElementsByClassName("description")[0].innerHTML = uglySweater.description;
-document
-  .getElementById("uglySweater")
-  .getElementsByClassName("price")[0].innerHTML = uglySweater.price;
+document.getElementById("uglySweater").getElementsByClassName("name")[0].innerHTML = uglySweater.name;
+document.getElementById("uglySweater").getElementsByClassName("category")[0].innerHTML = uglySweater.category;
+document.getElementById("uglySweater").getElementsByClassName("description")[0].innerHTML = uglySweater.description;
+document.getElementById("uglySweater").getElementsByClassName("price")[0].innerHTML = uglySweater.price;
 
-document
-  .getElementById("ornament")
-  .getElementsByClassName("name")[0].innerHTML = ornament.name;
-document
-  .getElementById("ornament")
-  .getElementsByClassName("category")[0].innerHTML = ornament.category;
-document
-  .getElementById("ornament")
-  .getElementsByClassName("description")[0].innerHTML = ornament.description;
-document
-  .getElementById("ornament")
-  .getElementsByClassName("price")[0].innerHTML = ornament.price;
+document.getElementById("ornament").getElementsByClassName("name")[0].innerHTML = ornament.name;
+document.getElementById("ornament").getElementsByClassName("category")[0].innerHTML = ornament.category;
+document.getElementById("ornament").getElementsByClassName("description")[0].innerHTML = ornament.description;
+document.getElementById("ornament").getElementsByClassName("price")[0].innerHTML = ornament.price;
 
-document
-  .getElementById("vinylRecord")
-  .getElementsByClassName("name")[0].innerHTML = vinylRecord.name;
-document
-  .getElementById("vinylRecord")
-  .getElementsByClassName("category")[0].innerHTML = vinylRecord.category;
-document
-  .getElementById("vinylRecord")
-  .getElementsByClassName("description")[0].innerHTML = vinylRecord.description;
-document
-  .getElementById("vinylRecord")
-  .getElementsByClassName("price")[0].innerHTML = vinylRecord.price;
+document.getElementById("vinylRecord").getElementsByClassName("name")[0].innerHTML = vinylRecord.name;
+document.getElementById("vinylRecord").getElementsByClassName("category")[0].innerHTML = vinylRecord.category;
+document.getElementById("vinylRecord").getElementsByClassName("description")[0].innerHTML = vinylRecord.description;
+document.getElementById("vinylRecord").getElementsByClassName("price")[0].innerHTML = vinylRecord.price;
 
 document.getElementById("socks").getElementsByClassName("name")[0].innerHTML = socks.name;
-document
-  .getElementById("socks")
-  .getElementsByClassName("category")[0].innerHTML = socks.category;
-document
-  .getElementById("socks")
-  .getElementsByClassName("description")[0].innerHTML = socks.description;
-  document.getElementById("socks").getElementsByClassName("price")[0].innerHTML = socks.price;
+document.getElementById("socks").getElementsByClassName("category")[0].innerHTML = socks.category;
+document.getElementById("socks").getElementsByClassName("description")[0].innerHTML = socks.description;
+document.getElementById("socks").getElementsByClassName("price")[0].innerHTML = socks.price;
 
-document
-  .getElementById("lumpOfCoal")
-  .getElementsByClassName("name")[0].innerHTML = lumpOfCoal.name;
-document
-  .getElementById("lumpOfCoal")
-  .getElementsByClassName("category")[0].innerHTML = lumpOfCoal.category;
-document
-  .getElementById("lumpOfCoal")
-  .getElementsByClassName("description")[0].innerHTML = lumpOfCoal.description;
-document
-  .getElementById("lumpOfCoal")
-  .getElementsByClassName("price")[0].innerHTML = lumpOfCoal.price;
+document.getElementById("lumpOfCoal").getElementsByClassName("name")[0].innerHTML = lumpOfCoal.name;
+document.getElementById("lumpOfCoal").getElementsByClassName("category")[0].innerHTML = lumpOfCoal.category;
+document.getElementById("lumpOfCoal").getElementsByClassName("description")[0].innerHTML = lumpOfCoal.description;
+document.getElementById("lumpOfCoal").getElementsByClassName("price")[0].innerHTML = lumpOfCoal.price;
 
-document
-  .getElementById("coffeeMug")
-  .getElementsByClassName("name")[0].innerHTML = coffeeMug.name;
-document
-  .getElementById("coffeeMug")
-  .getElementsByClassName("category")[0].innerHTML = coffeeMug.category;
-document
-  .getElementById("coffeeMug")
-  .getElementsByClassName("description")[0].innerHTML = coffeeMug.description;
-document
-  .getElementById("coffeeMug")
-  .getElementsByClassName("price")[0].innerHTML = coffeeMug.price;
+document.getElementById("coffeeMug").getElementsByClassName("name")[0].innerHTML = coffeeMug.name;
+document.getElementById("coffeeMug").getElementsByClassName("category")[0].innerHTML = coffeeMug.category;
+document.getElementById("coffeeMug").getElementsByClassName("description")[0].innerHTML = coffeeMug.description;
+document.getElementById("coffeeMug").getElementsByClassName("price")[0].innerHTML = coffeeMug.price;
 
-document
-  .getElementById("whiteElephant")
-  .getElementsByClassName("name")[0].innerHTML = whiteElephant.name;
+document.getElementById("whiteElephant").getElementsByClassName("name")[0].innerHTML = whiteElephant.name;
 document
   .getElementById("whiteElephant")
   .getElementsByClassName("category")[0].innerHTML = whiteElephant.category;
